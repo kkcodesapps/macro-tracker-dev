b975dd3d0baea43ca7dbc166691d95c893e33761
kkcodes <koliokolevcodes@gmail.com>
2024-10-24 03:04:51 -0500

    fixed button sizing

--- src/App.tsx
+++ src/App.tsx
@@ -114,7 +114,7 @@ function AppContent() {
             </Link>
             <Link
               to="/add-meal"
-              className={`flex items-center justify-center w-10 h-10 rounded-full ${
+              className={`flex items-center justify-center w-12 h-12 rounded-full ${
                 location.pathname === "/add-meal"
                   ? "bg-blue-500 text-white"
                   : darkMode
